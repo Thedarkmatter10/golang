3a9b20ded26f0fe8619be0d85d568928b65c1cd4
Thedarkmatter10 <ssatyamsingh1010@gmail.com>
2022-07-27 18:51:36 +0530

    test

--- slices.go
+++ slices.go
@@ -1,14 +1,18 @@
+
 package main
 
 import "fmt"
 
-func main(){
-	var a[5] int {"1,2,3,4,5"}
-		
+func main() {
+
 	
-	
-	fmt.Println(a)
-	
-		
-	
+	my_arr1 := [5]string{"Scala", "Perl", "Java", "Python", "Go"}
+
+	my_arr2 := my_arr1
+
+	fmt.Println("Array_1: ", my_arr1)
+	fmt.Println("Array_2:", my_arr2)
+	my_arr1[0] = "C++"
+	fmt.Println("\nArray_1: ", my_arr1)
+	fmt.Println("Array_2: ", my_arr2)
 }
